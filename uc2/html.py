b1062bfc5d2139202ea276fc969145170e9a3190
TnzinDEV <tnzin.github@gmail.com>
2026-09-18 18:42:52 -0300

    remaster commit

---
+++ uc2/html.py
@@ -0,0 +1,103 @@
+#Aula de html css e anotaçoes
+#Baixar live server para rodar as paginas em html em tempo real
+#criar um arquivo chamado index.html
+#html e o que da estrutura a pagina
+#emmet sao atalhos se voce der ! e enter ele te fornece o seguinte codigo
+
+
+#<!DOCTYPE html>
+#<html lang="en">
+#<head>
+#    <meta charset="UTF-8">
+#    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#    <title>Document</title>
+#</head>
+#<body>
+    
+#</body>
+#</html>
+
+
+#mudar para pt-br
+
+#tudo dentro do body aparece no site
+#se voceclickar em go live no canto inferior direito da tela do vscode
+#voce abre uma previa do seu html
+
+
+
+#Todas as tags ficam entre <nome da tag>
+
+#          tags
+
+#tag - h1 (da um titulo)
+#vai da h1 a h6 
+#quanto menor o numero e menor a letra
+#sao apenas para titulos e sub titulos
+
+#<p>- para paragrafo
+
+#<header> - header e a entrada do site
+#demonstraçao ou apresentaçao
+
+#o site funciona como blocos 
+#no codigo parecem continuos porem e importante organizar
+
+#O cabeçalio fica dentro de <header>
+
+# a tag <nav> serve para navegaçao
+
+#<ul> lista nao coordenada
+
+#<hr> - faz uma linha
+
+#<main> - e o conteudo principal
+
+#<section> - sessao (ficam dentro da main)
+
+#<br/> = break
+
+#<!-- documentaçao e comentario--> - para comentar
+
+
+
+
+#                links
+
+
+#<a href="qualquer link"> - ativa o seu link a de ativaçao
+#target="_blank abrir nova guia
+#identificaçao <section id="primeira">
+#para chamar <li><a href="#primeira"
+
+
+
+
+
+#                CSS
+# e a beleza
+
+#para mecher em uma tag especifica porem no codigo inteiro voce faz o seguinte
+#dentro da <head> voce faz o seguinte passo
+
+#<style>
+# tag.exemple {
+#   oque voce quer mexer por exemplo 
+#   cor 
+#    fonte 
+#      e etc
+#
+#</style>
+
+#E sempre bom voce abrir um arquivo style.css separado para caregar o seu css la
+# ja no seu codigo voce poem esta tag por exemplo 
+#<link rel="stylesheet" href="style.css">
+#todo seu css fica dentro de uma folder "css"
+#dentro de uma file que sera chamada
+
+
+#para por imagem no site segue o seguinte codigo
+
+#<img src="link da foto" alt="algum comentario"
+https://placehold.co/600x400/png
+#quando voce quer passar o mouse em cima e der alguma animaçao voce digita &hover
